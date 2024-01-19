dacf1a2ea402b2ff26d8848d9f8969081fafc7cb
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-01-19 16:10:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+
+
+SESSION1 = getenv("SESSION1", "")
+SESSION2 = getenv("SESSION2", "")
+SESSION3 = getenv("SESSION3", "")
+SESSION4 = getenv("SESSION4", "")
+SESSION5 = getenv("SESSION5", "")
+SESSION6 = getenv("SESSION6", "")
+SESSION7 = getenv("SESSION7", "")
+SESSION8 = getenv("SESSION8", "")
+SESSION9 = getenv("SESSION9", "")
+SESSION10 = getenv("SESSION10", "")
+SESSION11 = getenv("SESSION11", "")
+SESSION12 = getenv("SESSION12", "")
+SESSION13 = getenv("SESSION13", "")
+SESSION14 = getenv("SESSION14", "")
+SESSION15 = getenv("SESSION15", "")
+SESSION16 = getenv("SESSION16", "")
+SESSION17 = getenv("SESSION17", "")
+SESSION18 = getenv("SESSION18", "")
+SESSION19 = getenv("SESSION19", "")
+SESSION20 = getenv("SESSION20", "")
