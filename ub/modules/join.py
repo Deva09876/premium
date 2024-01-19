aa40866fc82bc104eb9f20981e369da0f50b5376
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-01-19 16:38:47 +0530

    Update join.py

--- ub/modules/join.py
+++ ub/modules/join.py
@@ -2,3 +2,8 @@ from ub import clients, app
 
 from pyrogram import filters
 from pyrogram.types import Message
+import config 
+
+@app.on_message(filters.command("join") & filters.user(config.SUDOERS))
+async def joinchat(app, m: Message):
+  
