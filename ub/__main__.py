b510c812e63dd8c53d2cd949421f555d7b9f9927
Dev2604 <144546659+Dev2604@users.noreply.github.com>
2024-01-19 10:34:51 +0530

    Create __main__.py

---
+++ ub/__main__.py
@@ -0,0 +1,24 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from ub.modules import ALL_MODULES
+from ub import clients, app, ids
+
+async def start_bot():
+    await app.start()
+    print("LOG: Founded Bot token Booting..")
+    for all_module in ALL_MODULES:
+        importlib.import_module("ub.modules" + all_module)
+        print(f"Successfully Imported {all_module} 💥")
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()
+            print(f"Started {ex.first_name} 🔥")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
