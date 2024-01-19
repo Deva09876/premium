e1649e5f69f03fda99b769963b4d3956fa56ad31
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-01-19 17:13:09 +0530

    Update join.py

--- ub/modules/join.py
+++ ub/modules/join.py
@@ -6,4 +6,13 @@ import config
 
 @app.on_message(filters.command("join") & filters.user(config.SUDOERS))
 async def joinchat(app, m: Message):
-  
+  chat=m.text.split(" ", 1)[1]
+  no=[]
+  for client in clients:
+    try:
+      await client.join_chat(chat)
+      await message.reply_text(f"{client.me.mention} joined @{chat}")
+    except Exception as e:
+      print(e)
+      continue 
+  await message.reply_text("Done")
