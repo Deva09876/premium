1df00e815404225cb1a36a75d6bcdec36ac4a1f2
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-01-19 16:06:04 +0530

    Update join.py

--- ub/modules/join.py
+++ ub/modules/join.py
@@ -1,1 +1,19 @@
-k
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+SESSION1 = getenv("SESSION1", "")
+SESSION2 = getenv("SESSION2", "")
+SESSION3 = getenv("SESSION3", "")
+SESSION4 = getenv("SESSION4", "")
+SESSION5 = getenv("SESSION5", "")
+SESSION6 = getenv("SESSION6", "")
+SESSION7 = getenv("SESSION7", "")
+SESSION8 = getenv("SESSION8", "")
+SESSION9 = getenv("SESSION9", "")
+SESSION10 = getenv("SESSION10", "")
