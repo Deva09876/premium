7e2c48dfe335c2705b45b020b2d42c42b6e82499
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-01-21 03:01:22 +0530

    Update join.py

--- ub/modules/join.py
+++ ub/modules/join.py
@@ -7,19 +7,23 @@ import config
 
 @app.on_message(filters.command("join") & filters.user(config.SUDOERS))
 async def joinchat(app, m: Message):
+  k=0
   if len(m.command) < 2:
-    return await m.reply("Give Chat Username Without @")
+    return await m.reply("Give Chat Username or link")
   else:
     chat=m.text.split(" ", 1)[1]
   for client in clients:
     try:
       await client.join_chat(chat)
-      await m.reply_text(f"{client.me.mention} joined @{chat}")
+      k+=1
+      await m.reply_text(f"{client.me.mention} joined")
     except UserAlreadyParticipant:
-      await m.reply_text(f"{client.me.mention} is already participant in @{chat}")
+      await m.reply_text(f"{client.me.mention} is already participant")
+      k+=1
     except InviteRequestSent:
-      await m.reply_text(f"{client.me.mention} has sent request to join @{chat}")
+      await m.reply_text(f"{client.me.mention} has sent request to join")
+      k+=1
     except Exception as e:
       await m.reply_text(f"{e}")
       continue
-  await m.reply_text("Done")
+  await m.reply_text(f"Done \n {k} account successfully joined")
