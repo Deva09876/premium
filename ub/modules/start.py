7d52251ffd63e229d7f7ee8ce40280ade3216809
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-01-20 12:09:06 +0530

    Update start.py

--- ub/modules/start.py
+++ ub/modules/start.py
@@ -1,7 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from ub import app
+from ub import app, clients
 
 @app.on_message(filters.command("start"))
 async def start(app, m: Message):
-  await m.reply_text("I'm Alive")
+  for client in clients:
+  await m.reply_text("I'm Alive\n\n{client.me.mention} is alive")
