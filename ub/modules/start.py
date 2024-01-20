94940589bd9eb770e3541f85481d54c83f4feab6
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-01-20 12:19:17 +0530

    Update start.py

--- ub/modules/start.py
+++ ub/modules/start.py
@@ -4,6 +4,6 @@ from ub import app, clients
 
 @app.on_message(filters.command("start"))
 async def start(app, m: Message):
+  await m.reply_text("I'm Alive\n\n")
   for client in clients:
-    await m.reply_text("I'm Alive\n\n")
     await m.reply_text(f"{client.me.mention} is alive")
