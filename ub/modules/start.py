ede9b8463fbc3607359a622a443982385df4c59c
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-01-21 12:58:32 +0530

    Update start.py

--- ub/modules/start.py
+++ ub/modules/start.py
@@ -5,10 +5,7 @@ from ub import app, clients
 @app.on_message(filters.command("start"))
 async def start(app, m: Message):
   k=0
-  try:
-    await m.reply_text("I'm Alive\n\n")
-    for client in clients:
-      k+=1
-  except:
-    continue
+  await m.reply_text("I'm Alive\n\n")
+  for client in clients:
+    k+=1
   await m.reply(f"Out Of {len(clients)} Clients, {k} Are Alive!")
